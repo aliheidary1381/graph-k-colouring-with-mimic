75f41fe402847c9d35b263c1b04d87df7ef47c55
parniame <109746477+parniame@users.noreply.github.com>
2022-12-09 00:10:09 +0330

    Complete main.py

--- main.py
+++ main.py
@@ -1,38 +1,39 @@
 from graph import *
 import numpy as np
-
+import pandas as pd
 k = 3
+def rootP(population : list[Graph],root: Vertex):
+    colours = [] # 0 1 2 ....
+    probability = []
+    for instance in population:
+        colours.append(instance.V[root.label].colour)
+    for colour in range(0,k+1):
+        probability.append(colours.count(colour)/(len(colours)))
+    
+    return probability
+def ConditionalProbability(population : list[Graph],children:Vertex,parent:Vertex):
+    colours = pd.DataFrame(np.zeros((k,k))) 
+    for instance in population:
+        colours[instance.V[parent.label].colour][instance.V[children.label].colour] +=1
+    Probability = colours.apply(lambda row : row/row.sum(),axis= 0)
+    return probability
 
-
-def rootP(population: list[Graph], root: Vertex):
-	colours = []  # 0 1 2 ....
-	probability = []
-	for instance in population:
-		colours.append(instance.V[root.id].colour)
-	for colour in range(0, k + 1):
-		probability.append(colours.count(colour) / (len(colours)))
-
-	return probability
-
-
-def ConditionalProbability(population: list[Graph], children: Vertex, parent: Vertex):
-	colours = np.zeros((k, k))
-	probability = np.zeros((k, k))
-	for instance in population:
-		colours[instance.V[parent].colour][instance.V[children].colour] += 1  # bug!
-	for parentColour in range(0, k + 1):
-		for childColour in range(0, k + 1):
-			probability[parentColour][childColour] = colours[parentColour][childColour] / (colours[parentColour].sum())
-	return probability
-
-
-def betterHalf(population: list[Graph], costs: list[int]):
-	dictionary = dict(zip(population, costs))
-	sortedG = sorted(dictionary.items(), key=lambda x: x[1])
-	halfG = dict(sortedG[:int(len(population) / 2)])  # bug!
-	return halfG.keys()
-
-
+def betterHalf(population:list[Graph],costs : list[int]):
+    dictionary = dict(zip(population,costs))
+    sortedG = sorted(dictionary.items(),key=lambda x: x[1])
+    halfG = dict(sortedG[:int(len(population)/2)])
+    return halfG.keys()
+def mutual_information(population:list[Graph],x:Vertex , y : Vertex):
+    colours = pd.DataFrame(np.zeros((k,k))) 
+    for instance in population:
+        colours[instance.V[y.label].colour][instance.V[x.label].colour] +=1
+    JointP = colours.apply(lambda x: x/len(population))
+    Yprobability = JointP.sum(axis=0)
+    temp = JointP/(Yprobability)#Yprobability
+    temp = temp.apply(lambda row : row/row.sum(),axis=1) #Xprobability
+    temp = np.log(temp)
+    MI = (temp*JointP).values.sum()
+    return MI
 G = Graph()
 G.add_vertex()
 G.add_vertex()
